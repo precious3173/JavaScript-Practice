affcb398dd620f036fd90238425a9e24968c3056
Admin <preciouspeter3173@gmail.com>
2023-07-02 18:45:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+let a = 7;
+if(a <5) console.log(" A is less");
+
+else        console.log(" A is more");
+
+let person = {
+    name: "Precious",
+    age: 40
+};
+       
+person.age = " john"
+console.log(person)
